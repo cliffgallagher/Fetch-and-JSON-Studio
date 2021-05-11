5515f3a3c32023d5442a822abae4d5473a76d40f
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-05-10 19:36:17 -0500

    fetches json astronaut object

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // TODO: add code here
+function init() {
+    //console.log("hello poppet");
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            //console.log(json);
+            const container = document.getElementById("container");
+            for (let i = 0; i < json.length; i++) {
+                //console.log(i);
+                container.innerHTML
+            }
+        });
+    });
+}
+
+window.addEventListener("load", init)
