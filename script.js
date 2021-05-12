6c2a91d6e970e3a8c247447c9f53c79042af3314
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-05-12 06:24:02 -0500

    about to add skills

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function init() {
     //console.log("hello poppet");
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json) {
-            //console.log(json);
+            console.log(json);
             const container = document.getElementById("container");
             for (let i = 0; i < json.length; i++) {
                 
@@ -15,11 +15,11 @@ function init() {
 
                 astronautDiv.innerHTML = `
                     <div class="bio">
-                        <h3>Mae Jemison</h3>
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
                         <ul>
-                            <li>Hours in space: 190</li>
-                            <li>Active: false</li>
-                            <li>Skills: Physician, Chemical Engineer</li>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li>Active: ${json[i].active}</li>
+                            <li>Skills: </li>
                         </ul>
                     </div>
                     <img class="avatar" src="images/mae-jemison.jpg">
