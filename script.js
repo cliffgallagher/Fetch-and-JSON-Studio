3a1dee5f97a5af6299379af76bbda661489d4d5b
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-05-11 20:15:34 -0500

    for loop adds template div appropriate number of times

--- script.js
+++ script.js
@@ -6,10 +6,30 @@ function init() {
             //console.log(json);
             const container = document.getElementById("container");
             for (let i = 0; i < json.length; i++) {
-                //console.log(i);
+                
+                let astronautDiv = document.createElement("div");
+                astronautDiv.classList.add("astronaut");
+
+                /*let bioDiv = document.createElement("div");
+                bioDiv.classList.add("bio");*/
+
+                astronautDiv.innerHTML = `
+                    <div class="bio">
+                        <h3>Mae Jemison</h3>
+                        <ul>
+                            <li>Hours in space: 190</li>
+                            <li>Active: false</li>
+                            <li>Skills: Physician, Chemical Engineer</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="images/mae-jemison.jpg">
+                `;
+                container.appendChild(astronautDiv);
+                
+                /*console.log(i);
                 let newText = document.createElement("p");
                 newText.innerHTML = "test";
-                container.appendChild(newText);
+                container.appendChild(newText);*/
             }
         });
     });
